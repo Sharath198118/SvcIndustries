ff0d067239deab6a4726accde4740bed6733d317
Sharath198118 <esharathkumarreddy@gmail.com>
2025-03-16 09:25:39 +0530

    Updates

--- svcindustries/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import SvcIndustriesDashboard from './components/SvcIndustriesDashboard'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <SvcIndustriesDashboard />
-  </StrictMode>,
-)
